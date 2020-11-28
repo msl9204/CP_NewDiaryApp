383b436469a93b61d093e5776cf91aefc24beb51
kimsejin <kimsejin@kimsejinui-MacBookPro.local>
2020-11-28 13:14:01 +0900

    main

--- app/src/main/java/com/example/newdiaryapp/MainActivity.java
+++ app/src/main/java/com/example/newdiaryapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.newdiaryapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String[] title = {"오늘은 야근을했다.", "남자친구가 과제해달라구한다.", "날씨가 너무 춥다.", "회사가 너무 멀다", "9호선 지옥철.."};
+
+        ListView list = (ListView) findViewById(R.id.mainListView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, title);
+
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),Detail.class);
+                intent.putExtra("title",title);
+                startActivity(intent);
+            }
+        });
     }
 }
