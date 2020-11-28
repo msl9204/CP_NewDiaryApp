3c50f5951c90bf35c0a767d181e4cb9b111deb11
MinSoo Lee <minsoo.lee@8byte.co.kr>
2020-11-28 11:25:48 +0900

    201128 이민수 Editor 기초 소스 추가

---
+++ app/src/main/java/com/example/newdiaryapp/Editor.java
@@ -0,0 +1,15 @@
+package com.example.newdiaryapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Editor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
+}
